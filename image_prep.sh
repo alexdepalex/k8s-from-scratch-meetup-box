efadd6b6c94d1a6a311455f59a009493d0033ea4
Fai Fung <ffung@xebia.com>
2016-10-13 23:46:35 +0200

    add weave binary, upgrade to 1.7.2

--- image_prep.sh
+++ image_prep.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 docker pull gcr.io/google_containers/hyperkube:v1.4.0
-docker pull weaveworks/weave-kube:latest
-docker pull weaveworks/weave-npc:latest
+docker pull gcr.io/google_containers/pause-amd64:3.0
+docker pull weaveworks/weave-kube:1.7.2
+docker pull weaveworks/weave-npc:1.7.2
+
 docker pull weaveworks/plugin:1.7.0
 docker pull weaveworks/weaveexec:1.7.0
 docker pull weaveworks/weave:1.7.0
@@ -13,5 +15,6 @@ sudo mkdir -p /opt/bin
 sudo curl -L -o /opt/bin/kubelet -z /opt/bin/kubelet https://storage.googleapis.com/kubernetes-release/release/v1.4.0/bin/linux/amd64/kubelet
 sudo curl -L -o /opt/bin/kubectl -z /opt/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.4.0/bin/linux/amd64/kubectl
 sudo curl -L -o /opt/bin/kube-proxy -z /opt/bin/kube-proxy https://storage.googleapis.com/kubernetes-release/release/v1.4.0/bin/linux/amd64/kube-proxy
+sudo curl -L git.io/weave -o /opt/bin/weave
 
 sudo chmod +x /opt/bin/*
